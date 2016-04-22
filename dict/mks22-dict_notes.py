3e44851367904a5f13de06d7c03835b12e943c4b
Jason Kao <jkao1@stuy.edu>
2016-04-22 13:27:32 -0400

    finished friday() v.1, working on the alphabetically ordered one

--- dict/mks22-dict_notes.py
+++ dict/mks22-dict_notes.py
@@ -1,4 +1,4 @@
-def f():
+def thursday():
     f = open('file.txt', 'rU')
     s = f.read()
     f.close()
@@ -11,3 +11,15 @@ def f():
         output[thing] = row[title.index(thing)]
     return output
     
+def friday():
+    f = open('p8.csv', 'rU')
+    s = f.read()
+    f.close()
+    L = s.split('\n')[1:]
+    for elem in L:
+        L[L.index(elem)] = elem.split(',')
+    for bro in L:
+        if 'MA' in bro[1] + bro[0]:
+            print(bro[1]+' '+bro[0] + ' has a rating of ' + bro[2])
+
+friday()
