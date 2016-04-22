1c641bc25a7d873959ca4d1b0fcca9a11b396eb3
Jason Kao <jkao1@stuy.edu>
2016-04-22 13:38:12 -0400

    finished functions for today. yay.

--- dict/mks22-dict_notes.py
+++ dict/mks22-dict_notes.py
@@ -1,4 +1,4 @@
-def thursday():
+def t():
     f = open('file.txt', 'rU')
     s = f.read()
     f.close()
@@ -11,15 +11,19 @@ def thursday():
         output[thing] = row[title.index(thing)]
     return output
     
-def friday():
+def f():
     f = open('p8.csv', 'rU')
     s = f.read()
     f.close()
     L = s.split('\n')[1:]
     for elem in L:
         L[L.index(elem)] = elem.split(',')
+    output = []
     for bro in L:
-        if 'MA' in bro[1] + bro[0]:
-            print(bro[1]+' '+bro[0] + ' has a rating of ' + bro[2])
+        if 'MA' == bro[1][:2] or 'MA' == bro[0][:2]:
+            output.append(bro[1]+' '+bro[0] + ' has a rating of ' + bro[2])
+    output.sort()
+    for hi in output:
+        print(hi + '.')
 
-friday()
+f()
