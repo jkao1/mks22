8a9a62c5b8cdfc23e386a8d04af1af3a47e5f56c
Jason Kao <jkao1@stuy.edu>
2016-04-23 05:06:33 +0800

    uh

--- dict/mks22-hw0502.py
+++ dict/mks22-hw0502.py
@@ -1,3 +1,4 @@
+#1 (solved)
 def ReadSAT(filename):
     f = open(filename,'rU')
     s = f.read()
@@ -5,17 +6,102 @@ def ReadSAT(filename):
     L = s.split('\n')[1:-1] # empty space after last line break
     for elem in L:
         L[L.index(elem)] = elem.split(',')
-    print(L)
+    return L
 
+print(ReadSAT('sat10-small.csv'))
+print('\n')
+
+#2 (solved)
 def HighLowSAT(filename):
     f = open(filename,'rU')
     s = f.read()
     f.close()
-    L = s.split('\n')[1:-1] # empty space after last line break
-    for elem in L:
-        if '"' in elem:
-            elem = elem[:elem.find('"')] + elem[elem.find('"')+1:]
-            target = elem.rfind(',', 0, elem.find('"'))
-            elem = elem[:target] + elem[target+1:]
-        L[L.index(elem)] = elem.split(',')
-    print(L)
+    L = s.split('\n')[1:-1]
+    for i in range(len(L)):
+        if '"' in L[i]:
+            quote = L[i].find('"') 
+            target = L[i].find(',',quote) # finds the first comma after double quote
+            noComma = L[i][:target] + L[i][target+1:]
+            L[i] = noComma.split(',')
+        else:
+            L[i] = L[i].split(',')
+    return L
+
+print(HighLowSAT('sat10-small.csv'))              
+print('\n')
+
+#3 (solved)
+def BigStats(filename):
+    f = open(filename,'rU')
+    s = f.read()
+    f.close()
+    L = s.split('\n')[1:-1]
+    numStudents = 0
+    totalRead = 0
+    totalMath = 0
+    totalWrite= 0
+    for i in range(len(L)):
+        if '"' in L[i]:
+            quote = L[i].find('"') 
+            target = L[i].find(',',quote) # finds the first comma after double quote
+            noComma = L[i][:target] + L[i][target+1:]
+            L[i] = noComma.split(',')
+        else:
+            L[i] = L[i].split(',')
+        try:
+            numStudents += int(L[i][2])
+            totalRead += int(L[i][3])
+            totalMath += int(L[i][4])
+            totalWrite += int(L[i][5])
+        except ValueError: #-->can't int() a string
+            numStudents += 0
+    print("Total students: " + str(numStudents))
+    print("Avg reading score: " + str(totalRead / numStudents))
+    print("Avg math score: " + str(totalMath / numStudents))
+    print("Avg writing score: " + str(totalWrite / numStudents))
+
+BigStats('sat10.csv')
+print('\n')
+
+#4 (in proogress)
+def School2Dict(line):
+    if '"' in line:
+        quote = line.find('"') 
+        target = line.find(',',quote)
+        line = (line[:target] + line[target+1:]).replace('"','')
+    line = line.split(',')
+    cg = ['DBN','Name','Number','Reading','Math','Writing']
+    output = {}
+    for i in range(len(line)):
+        output[cg[i]] = line[i]
+    return output
+        
+#5 (in progress)
+def MakeMaster(filename):
+    f = open(filename,'rU')
+    s = f.read()
+    f.close()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
