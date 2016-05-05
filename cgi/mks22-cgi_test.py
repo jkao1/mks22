f4d19d1c4eee2d6f37920a9ecb561de35ef5b673
Jason Kao <jkao1@stuy.edu>
2016-05-04 22:41:16 -0400

    uh

---
+++ cgi/mks22-cgi_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import cgi
+
+import cgitb
+cgitb.enable()
+
+HTML_HEADER = 'Content-type:text/html\r\n\r\n'
+
+Top_HTML = """
+<html>
+<head>
+<title>CGI Test 2</title>
+</head>
+<body>
+<b>Nihao Jason</b>
+<p>
+"""
+
+Bottom_HTML = "</body></html>"
+
+def doit():
+    elements = cgi.FieldStorage()
+    print(HTML_HEADER + Top_HTML)
+    print(elements)
+    print(Bottom_HTML)
+
+doit()
