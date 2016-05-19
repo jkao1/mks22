cb26bc94c8331cfcc00eff0c37423607107cfd3c
jkao1 <jkao1@stuy.edu>
2016-05-19 13:48:23 -0400

    5/19 class

---
+++ finalproj/README.md
@@ -0,0 +1,16 @@
+#final proj
+
+###Ideas
+Project Proposal:
+Problem: story abt starbucks and searching for nutrition, going back three directories for every item, too tedious
+Solution: python program parses every
+###To-dos
+
+###Notes
+```html
+<form method="POST" action="filename.py">
+    ...
+    <!--maintains a "session", keeps info for every individual-->
+    <input type="hidden" name=id value="account">
+</form>
+```
--- finalproj/testing.py
+++ finalproj/testing.py
@@ -1,14 +1,7 @@
-import urllib.request
+import urllib
 
-url = "http://www.starbucks.com/menu/drinks/espresso/"
-end = "?foodZone=9999"
-
-def f(drink):
-    link = url + drink + end
-    response = urllib.request.urlopen(link)
-    html = response.read()
-    html = html.decode(encoding='utf-8')
-    t_start = html.rfind('class="nutrition"')
-    t_end = html.find('</table>',t_start)
-    raw = html[t_start:t_end].replace('\r','').replace('\n','').replace('\t','')
-    
+def b():
+    url_nutr = 'http://www.starbucks.com/menu/catalog/nutrition?drink=espresso#view_control=nutrition'
+    f = urllib.urlopen(url_nutr)
+    s = f.read()
+    table_loc = s.rfind('<table ')
