487507c9830c7debe5a186f142016a993309b86c
jkao1 <jkao1@stuy.edu>
2016-06-05 13:38:33 -0400

    works OK, still have to figure out how to get food title

--- finalproj/resources/jamba.py
+++ finalproj/resources/jamba.py
@@ -8,6 +8,11 @@ def jm(product):
     l_start = s.find('<div class="mainbody menu menu_listing">')
     l_end = s.find('<section id="Footer1_',l_start)
     listing = s[l_start:l_end]
+
+
+
+    
+    """
     listing = listing.split('<div class="updatePanel"')
 
     M = {}
@@ -18,11 +23,12 @@ def jm(product):
         output = output[o_start:o_end].replace('\r\n','')
 
         output = output.split('<td class="left_col"')
-        output = output[1:3]+output[6:10]
+        output = output[1:3]+output[6:11]+[output[-1]] # otherwise there are repeats of nutrition info
         t_container = output[-1] # item that has title
         t_start = t_container.rfind('>')+1
         food = t_container[t_start:].strip(' ')
         sub = {}
+        #return t_container
         for i in range(len(output)): # loops every nutrition info
             elem = output[i]
             l = elem.split(' ')
@@ -34,8 +40,9 @@ def jm(product):
                 num = l[1]
             if i == 0:
                 num = l[20]
-            print title+num
             sub[title.lower()] = num
         del sub['sugar']
-        return 'hi'
+
         M[food] = sub
+    return M
+    """
