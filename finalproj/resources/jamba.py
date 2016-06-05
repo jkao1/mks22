88753b061deed233c34e6eddaf45f6043f90076e
jkao1 <jkao1@stuy.edu>
2016-06-05 12:09:13 -0400

    working so far. fixing 'calories' issue rn

--- finalproj/resources/jamba.py
+++ finalproj/resources/jamba.py
@@ -1,7 +1,7 @@
 import urllib
 
-def jm():
-    url = 'http://www.jambajuice.com/menu-and-nutrition/menu/freshly-squeezed-juice-shots'
+def jm(product):
+    url = 'http://www.jambajuice.com/menu-and-nutrition/menu/'+product
     f = urllib.urlopen(url)
     s = f.read()
 
@@ -10,16 +10,20 @@ def jm():
     listing = s[l_start:l_end]
     listing = listing.split('<div class="updatePanel"')
 
-    for i in range(1,len(listing)):
+    M = {}
+    for i in range(1,len(listing)): # loops every food item
         output = listing[i]
         o_start = output.find('<tr class="table_line">')
-        o_end = output.find('<div class="percentages clearfix">')
+        o_end = output.find('<sup>')
         output = output[o_start:o_end].replace('\r\n','')
 
         output = output.split('<td class="left_col"')
-        output = output[1:3]+output[6:]
-        M = {}
-        for i in range(len(output)):
+        output = output[1:3]+output[6:10]
+        t_container = output[-1] # item that has title
+        t_start = t_container.rfind('>')+1
+        food = t_container[t_start:].strip(' ')
+        sub = {}
+        for i in range(len(output)): # loops every nutrition info
             elem = output[i]
             l = elem.split(' ')
             if l[1].isalpha():
@@ -30,6 +34,8 @@ def jm():
                 num = l[1]
             if i == 0:
                 num = l[20]
-            M[title.lower()] = num
-        del M['sugar']
-        print M
+            print title+num
+            sub[title.lower()] = num
+        del sub['sugar']
+        return 'hi'
+        M[food] = sub
